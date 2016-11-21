6a6b346940ad0feff725fe850a8854141695f4f0
Gustavo Landfried <gustavolandfried@gmail.com>
2016-11-21 09:07:45 -0300

    Cambio la función de disipación. Dejo una temperatura mínima de 0.1.

--- tatetitoDisipacion_main.py
+++ tatetitoDisipacion_main.py
@@ -15,21 +15,22 @@ with open('Modelo_entrenado_21dia_00hs.pk', 'rb') as in_strm:
 	mundo = dill.load(in_strm)
 
 timesLearn = 60*24
-halflife = (timesLearn)*2/3
+halflife = (timesLearn)/4
 disipacion=0.01
 b0=-halflife*disipacion
+
 
 d = datetime.now()
 while d.day < 23:
 	d = datetime.now()
 	t = d.hour*d.minute
-	temp = (1/(1+np.e**(-(b0+disipacion*(timesLearn-t ) ) ) ))
+	temp = 0.1+((1-0.1)/(1+np.e**(-(b0+disipacion*(timesLearn-t ) ) ) ))
 	mundo.learn(temp1=temp, temp2=temp)				
 dill.dump(mundo, file('Modelo_entrenado_23dia_00hs.pk','w'))
 while d.day < 22:
 	d = datetime.now()
 	t= d.hour*d.minute
-	temp = (1/(1+np.e**(-(b0+disipacion*(timesLearn-t ) ) ) ))	
+	temp = 0.1+((1-0.1)/(1+np.e**(-(b0+disipacion*(timesLearn-t ) ) ) ))
 	mundo.learn(temp1=temp, temp2=temp)				
 dill.dump(mundo, file('Modelo_entrenado_22dia_00hs.pk','w'))
 
